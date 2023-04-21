04ef5f675d8386769763b1831148391ff5d5fe37
LeoRussell <leo.russell@mail.ru>
2023-04-21 21:29:02 +0300

    added /settings to configure the bot, added the ability to select the translation language, corrected the names of the databases

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import requests
 import config
 from english_words import get_english_words_set
 import random
@@ -11,8 +10,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=config.prefix, intents=intents) 
 
-con_score = sqlite3.connect("scores.db")
-cur_score = con_score.cursor()
+con_ids = sqlite3.connect("ids.db")
+cur_ids = con_ids.cursor()
 
 con_words = sqlite3.connect("words.db")
 cur_words = con_words.cursor()
@@ -22,34 +21,80 @@ russianwords = cur_words.execute(f"""SELECT ru FROM translates""").fetchall()
 
 @bot.command()
 async def translate(ctx):
-    output = random.choice(englishwords)[0]
+    bot.remove_command("reply")
+    
+    if ctx.author not in cur_ids.execute(f"""SELECT id FROM options""").fetchall():
+        cur_ids.execute(f"""INSERT INTO options(id, language) VALUES('{ctx.author}', 'en')""")
 
-    answer = cur_words.execute(f'''SELECT ru FROM translates WHERE en = "{output}"''').fetchall()
+    options = [i[0] for i in cur_ids.execute(f"""SELECT language FROM options WHERE id = '{ctx.author}'""").fetchall()]
+
+
+    if "ru" in options:
+        output = random.choice(russianwords)[0]
+        answer = cur_words.execute(f'''SELECT en FROM translates WHERE ru = "{output}"''').fetchall()
+
+    else:
+        output = random.choice(englishwords)[0]
+        answer = cur_words.execute(f'''SELECT ru FROM translates WHERE en = "{output}"''').fetchall()
+
     answer = [i[0] for i in answer]
 
     await ctx.reply(output)
 
+    con_ids.commit()
     @bot.command()
-    async def reply(ctx, message):
-        message = message.lower().replace("ё", "")
-        if message in answer:
-            author_id = cur_score.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall()
-            if author_id == []:
-                cur_score.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
-                cur_score.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
-                points = 1
+    async def reply(ctx, message="", additional=""):
+        if message != "":
+            if additional != "":
+                message = message.lower().replace("ё", "") + " " + additional
             else:
-                points = int(cur_score.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0][0]) + 1
-                cur_score.execute(f"""UPDATE results SET points = {points} WHERE id = '{ctx.author}'""")
+                message = message.lower().replace("ё", "")
 
-            con_score.commit()
-            await ctx.reply(f"Верно! Ваш счёт: {points}.")
-            await ctx.reply(f"Возможные варианты перевода: {', '.join(answer)}.")
+            if message in answer:
+                if cur_ids.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall() == []:
+                    cur_ids.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
+                    cur_ids.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
+                    points = 1
+                else:
+                    points = int(cur_ids.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0][0]) + 1
+                    cur_ids.execute(f"""UPDATE results SET points = {points} WHERE id = '{ctx.author}'""")
 
+                con_ids.commit()
+                await ctx.reply(f"Верно! Ваш счёт: {points}.")
+
+            else:
+                await ctx.reply(f"Неверно! Возможные варианты перевода - {', '.join(answer)}")
+                
         else:
-            await ctx.reply(f"Неверно! Правильный ответ - {', '.join(answer)}")
+            await ctx.reply(f"Ошибка! Попробуйте ввести ответ ещё раз.")
+
+
+@bot.command()
+async def settings(ctx, *options):
+    if ctx.author not in cur_ids.execute(f"""SELECT id FROM options""").fetchall(): 
+        cur_ids.execute(f"""INSERT INTO options(id, language) VALUES('{ctx.author}', 'en')""")
+        con_ids.commit()
+    
+    p_options = cur_ids.execute(f"""SELECT language FROM options WHERE id = '{ctx.author}'""").fetchall()
+    if len(options) == 0 or options == []:
+        await ctx.reply(f"Ваши настройки:\n-language: {p_options[0][0]}.")
+
+    else:
+        if "-language" in options:
+            try:
+                if options[options.index("-language") + 1] == "ru":
+                    cur_ids.execute(f"""UPDATE options SET language = 'ru' WHERE id = '{ctx.author}'""")
+                elif options[options.index("-language") + 1] == "en":
+                    cur_ids.execute(f"""UPDATE options SET language = 'en' WHERE id = '{ctx.author}'""")
+                
+                con_ids.commit()
+                await ctx.reply(f"Настройки успешно изменены!")
             
-        bot.remove_command("reply")
+            except IndexError:
+                await ctx.reply(f"Ошибка! Попробуйте ввести настройки ещё раз.")
+
+
+
 
 if __name__ == "__main__":
     bot.run(config.token)
