bfb7cd0dab016fd75c0e01ddf6d975d641b06106
LeoRussell <leo.russell@mail.ru>
2023-04-21 16:59:30 +0300

    made some translation improvements

--- main.py
+++ main.py
@@ -10,52 +10,45 @@ web2lowerset = get_english_words_set(['web2'], lower=True)
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=config.prefix, intents=intents) 
-web2lowerset = ["bad", "good"]
 
-con = sqlite3.connect("scores.db")
-cur = con.cursor()
+con_score = sqlite3.connect("scores.db")
+cur_score = con_score.cursor()
+
+con_words = sqlite3.connect("words.db")
+cur_words = con_words.cursor()
+
+englishwords = cur_words.execute(f"""SELECT en FROM translates""").fetchall()
+russianwords = cur_words.execute(f"""SELECT ru FROM translates""").fetchall()
 
 @bot.command()
 async def translate(ctx):
-    output = random.choice(list(web2lowerset))
-    
-    
-    url = "https://microsoft-translator-text.p.rapidapi.com/translate"
+    output = random.choice(englishwords)[0]
 
-    querystring = {"to[0]":"ru","api-version":"3.0","profanityAction":"NoAction","textType":"plain"}
-
-    payload = [{"Text": output}]
-    headers = {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": "5ae9d6a133mshe6372b0ccf5e37bp10183ajsn73865becc77e",
-        "X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com"
-    }
-
-    response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
+    answer = cur_words.execute(f'''SELECT ru FROM translates WHERE en = "{output}"''').fetchall()
+    answer = [i[0] for i in answer]
 
     await ctx.reply(output)
 
     @bot.command()
     async def reply(ctx, message):
-        answer = response.json()[0]["translations"][0]["text"]
-        print(response.text)
-        if message == answer:
-            author_id = cur.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall()
-            print(author_id)
+        message = message.lower().replace("ё", "")
+        if message in answer:
+            author_id = cur_score.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall()
             if author_id == []:
-                cur.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
-                cur.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
+                cur_score.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
+                cur_score.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
                 points = 1
             else:
-                print(cur.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0])
-                points = int(cur.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0][0]) + 1
-                cur.execute(f"""UPDATE results SET points = {points} WHERE id = '{ctx.author}'""")
+                points = int(cur_score.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0][0]) + 1
+                cur_score.execute(f"""UPDATE results SET points = {points} WHERE id = '{ctx.author}'""")
 
-            con.commit()
-            await ctx.reply(f"Верно! Ваш счёт: {points}")
+            con_score.commit()
+            await ctx.reply(f"Верно! Ваш счёт: {points}.")
+            await ctx.reply(f"Возможные варианты перевода: {', '.join(answer)}.")
 
         else:
-            await ctx.reply(f"Неверно! Правильный ответ - {answer}")
+            await ctx.reply(f"Неверно! Правильный ответ - {', '.join(answer)}")
+            
         bot.remove_command("reply")
 
 if __name__ == "__main__":
