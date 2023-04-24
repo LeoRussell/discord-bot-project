f1158a38b4f1950b44d17f0b1be651efcf628b4c
LeoRussell <leo.russell@mail.ru>
2023-04-24 16:24:20 +0300

    added /words, but bot doesn't work at all

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+from english_words import get_english_words_set
 import config
 import random
 import sqlite3
 import asyncio
 import requests
+
 
 
 intents = discord.Intents.default()
@@ -17,15 +19,18 @@ cur_ids = con_ids.cursor()
 con_words = sqlite3.connect("words.db")
 cur_words = con_words.cursor()
 
+web2lowerset = get_english_words_set(['web2'], lower=True)
+
 englishwords = cur_words.execute(f"""SELECT en FROM translates""").fetchall()
 russianwords = cur_words.execute(f"""SELECT ru FROM translates""").fetchall()
 
 
-@bot.command
+@bot.command()
 async def start(ctx, message):
     bot.remove_command("reply") 
     if str(ctx.author) not in [i[0] for i in cur_ids.execute(f"""SELECT id FROM options""").fetchall()]:
         cur_ids.execute(f"""INSERT INTO options(id, language, mode, timer) VALUES('{ctx.author}', 'en', 'single', '5')""")
+        con_ids.commit()
     
     options = [i for i in cur_ids.execute(f"""SELECT language, mode, timer FROM options WHERE id = '{ctx.author}'""").fetchall()[-1]]
 
@@ -59,13 +64,12 @@ async def start(ctx, message):
                 if cur_ids.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall() == []:
                     cur_ids.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
                     cur_ids.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
-                    points = 1
                 else:
                     points = int(cur_ids.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0][0]) + 1
                     cur_ids.execute(f"""UPDATE results SET points = {points} WHERE id = '{ctx.author}'""")
 
                 con_ids.commit()
-                await ctx.reply(f"Верно! Ваш счёт: {points}.")
+                await ctx.reply(f"Верно!")
 
             else:
                 await ctx.reply(f"Неверно! Возможные варианты перевода - {', '.join(answer)}")
@@ -81,7 +85,7 @@ async def start(ctx, message):
     
 
     try:
-        m = await bot.wait_for("message", check=check, timeout = options[2])
+        m = await bot.wait_for("message", check=check, timeout = int(options[2]))
     except asyncio.TimeoutError:
         bot.remove_command("reply") 
         await ctx.send(f"{ctx.author.mention} Время ответа вышло!")
@@ -171,6 +175,11 @@ async def translate(ctx, *message):
 
 @bot.command()
 async def top(ctx, num=10):
+    if cur_ids.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall() == []:
+        cur_ids.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
+        cur_ids.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
+        con_ids.commit()
+
     results = cur_ids.execute(f"""SELECT id, points FROM results""").fetchall()
     dict_results = dict()
     for i in results:
@@ -191,6 +200,51 @@ async def top(ctx, num=10):
         n += 1
     
     await ctx.send(output)
+
+
+@bot.command()
+async def words(ctx):
+    bot.remove_command("reply")
+    if str(ctx.author) not in [i[0] for i in cur_ids.execute(f"""SELECT id FROM options""").fetchall()]:
+        cur_ids.execute(f"""INSERT INTO options(id, language, mode, timer) VALUES('{ctx.author}', 'en', 'single', '5')""")
+        con_ids.commit()
+
+    options = [i for i in cur_ids.execute(f"""SELECT language, mode, timer FROM options WHERE id = '{ctx.author}'""").fetchall()[-1]]
+
+
+    word = random.choice(list(web2lowerset))
+    await ctx.reply(word)
+    
+    def check(message):
+        return message.author.id == ctx.author.id
+    
+    @bot.command()
+    async def reply(ctx, answer):
+        if answer[0] == word[-1]:
+            ok = list(web2lowerset)
+            #print(word, word in ok)
+            #print(ok.index(word))
+            if word in list(web2lowerset):
+                if cur_ids.execute(f"""SELECT id FROM results WHERE id = '{ctx.author}'""").fetchall() == []:
+                    cur_ids.execute(f"""INSERT INTO results(id) VALUES('{ctx.author}')""")
+                    cur_ids.execute(f"""UPDATE results SET points = 1 WHERE id = '{ctx.author}'""")
+                else:
+                    points = int(cur_ids.execute(f"""SELECT points FROM results WHERE id = '{ctx.author}'""").fetchall()[0][0]) + 1
+                    cur_ids.execute(f"""UPDATE results SET points = {points} WHERE id = '{ctx.author}'""")
+                
+                con_ids.commit()
+
+                asyncio.run_coroutine_threadsafe(words(ctx), bot.loop)
+            else:
+                await ctx.send("Хм, этого слова нет в моей базе данных. Попробуй ещё раз!")
+        else:
+            await ctx.reply(f'Слово должно начинаться с полследней буквы "{word.upper()}"')
+
+    try:
+        m = await bot.wait_for("message", check=check, timeout = int(options[2]))
+    except asyncio.TimeoutError:
+        bot.remove_command("reply") 
+        await ctx.send(f"{ctx.author.mention} Время ответа вышло!")
 
 
 @bot.command()
